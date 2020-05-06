5c7ea3716f4b7cdc519a753242d86e237eed406a
Gyeong <chung3286@konkuk.ac.kr>
2020-05-06 13:11:49 +0900

    John Wallis

--- src/getPi/GetPi.java
+++ src/getPi/GetPi.java
@@ -2,15 +2,15 @@ package getPi;
 
 public class GetPi {
 	public static void main(String[] args) {
-		// Euler
-		int howmany = 2000000;
-		int n=1;
-		double pi2=0;
-		for(int i=0; i<howmany; i++) {
-			pi2 = pi2 + 1 / (Math.pow(n, 2));
+		// John Wallis
+		int howmany = 2000000, n=1;
+		double pi3=1;
+		
+		for(int i=1; i<howmany; i++) {
+			pi3 = pi3 * ((4*Math.pow(n, 2))/(4*Math.pow(n, 2)-1));
 			n++;
 		}
-		pi2 = Math.sqrt(pi2*6);
-		System.out.println(pi2);
+		pi3 *= 2;
+		System.out.println(pi3);
 	}
 }
